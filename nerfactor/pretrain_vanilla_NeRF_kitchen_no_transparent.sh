b2de9efacd157e3036d467fcaadcc29a121200db
changwoonchoi <zzzmaster@snu.ac.kr>
2023-05-10 21:49:58 +0900

    kitchen no transparent

--- nerfactor/pretrain_vanilla_NeRF_kitchen_no_transparent.sh
+++ nerfactor/pretrain_vanilla_NeRF_kitchen_no_transparent.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env/bash
 
-scene="kitchen"
+scene="kitchen_no_transparent"
 gpus='0'
-repo_dir='/home/ccw/project/cloned_repos/nerfactor'
-data_root="/home/ccw/project/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
+repo_dir='/home/ccw/cloned_repos/nerfactor'
+data_root="/home/ccw/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
 
 imh='360'
 lr='5e-4'
 
 near='0.143'; far='8.212'
-outroot="/home/ccw/project/cloned_repos/nerfactor/output/train/${scene}_nerf"
+outroot="/home/ccw/cloned_repos/nerfactor/output/train/${scene}_nerf"
 REPO_DIR="$repo_dir" "$repo_dir/nerfactor/trainvali_run.sh" "$gpus" --config='nerf.ini' --config_override="data_root=$data_root,imh=$imh,near=$near,far=$far,lr=$lr,outroot=$outroot"
 
 ckpt="$outroot/lr$lr/checkpoints/ckpt-10"
--- nerfactor/pretrain_vanilla_NeRF_living-room.sh
+++ nerfactor/pretrain_vanilla_NeRF_living-room.sh
@@ -2,14 +2,14 @@
 
 scene="living-room"
 gpus='1'
-repo_dir='/home/ccw/project/cloned_repos/nerfactor'
-data_root="/home/ccw/project/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
+repo_dir='/home/ccw/cloned_repos/nerfactor'
+data_root="/home/ccw/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
 
 imh='360'
 lr='5e-4'
 
 near='0.0'; far='6.340'
-outroot="/home/ccw/project/cloned_repos/nerfactor/output/train/${scene}_nerf"
+outroot="/home/ccw/cloned_repos/nerfactor/output/train/${scene}_nerf"
 REPO_DIR="$repo_dir" "$repo_dir/nerfactor/trainvali_run.sh" "$gpus" --config='nerf.ini' --config_override="data_root=$data_root,imh=$imh,near=$near,far=$far,lr=$lr,outroot=$outroot"
 
 ckpt="$outroot/lr$lr/checkpoints/ckpt-10"
