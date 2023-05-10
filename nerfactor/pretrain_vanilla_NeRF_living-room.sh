4312bdc9595a0d565fc090f355bbc35323ec239c
changwoonchoi <zzzmaster@snu.ac.kr>
2023-05-10 22:03:52 +0900

    fix bug: near!=0

--- nerfactor/pretrain_vanilla_NeRF_living-room.sh
+++ nerfactor/pretrain_vanilla_NeRF_living-room.sh
@@ -8,7 +8,7 @@ data_root="/home/ccw/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
 imh='360'
 lr='5e-4'
 
-near='0.0'; far='6.340'
+near='0.1'; far='6.340'
 outroot="/home/ccw/cloned_repos/nerfactor/output/train/${scene}_nerf"
 REPO_DIR="$repo_dir" "$repo_dir/nerfactor/trainvali_run.sh" "$gpus" --config='nerf.ini' --config_override="data_root=$data_root,imh=$imh,near=$near,far=$far,lr=$lr,outroot=$outroot"
 
