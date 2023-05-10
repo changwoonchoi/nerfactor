f6fcdd7ade355f0acdf1687ad9ff5bd9aadda434
changwoonchoi <changwoon.choi00@gmail.com>
2023-05-10 12:57:52 +0000

    fix bug: near!=0

--- nerfactor/pretrain_vanilla_NeRF_classroom.sh
+++ nerfactor/pretrain_vanilla_NeRF_classroom.sh
@@ -8,7 +8,7 @@ data_root="/home/ccw/project/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
 imh='360'
 lr='5e-4'
 
-near='0'; far='151.36'
+near='0.1'; far='151.36'
 outroot="/home/ccw/project/cloned_repos/nerfactor/output/train/${scene}_nerf"
 REPO_DIR="$repo_dir" "$repo_dir/nerfactor/trainvali_run.sh" "$gpus" --config='nerf.ini' --config_override="data_root=$data_root,imh=$imh,near=$near,far=$far,lr=$lr,outroot=$outroot"
 
--- nerfactor/pretrain_vanilla_NeRF_dining-room.sh
+++ nerfactor/pretrain_vanilla_NeRF_dining-room.sh
@@ -8,7 +8,7 @@ data_root="/home/ccw/project/cloned_repos/nerfactor/data/IBL-NeRF/${scene}"
 imh='360'
 lr='5e-4'
 
-near='0.0'; far='16.44'
+near='0.1'; far='16.44'
 outroot="/home/ccw/project/cloned_repos/nerfactor/output/train/${scene}_nerf"
 REPO_DIR="$repo_dir" "$repo_dir/nerfactor/trainvali_run.sh" "$gpus" --config='nerf.ini' --config_override="data_root=$data_root,imh=$imh,near=$near,far=$far,lr=$lr,outroot=$outroot"
 
